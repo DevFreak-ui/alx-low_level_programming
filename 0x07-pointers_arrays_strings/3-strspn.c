7d49faffb93d202c0926f2dbf6945bd5f0b17a34
Prince <mirekuprince18@gmail.com>
2022-09-26 21:41:48 -0700

    Correction

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,45 +1,32 @@
-
-Bodundeft
-/
-alx-low_level_programming
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-alx-low_level_programming/0x07-pointers_arrays_strings/3-strspn.c
-
-Your Name Even more pointers, arrays and strings_3-strspn.c
- 1 contributor
-28 lines (26 sloc)  460 Bytes
 #include "main.h"
 
 /**
- * _strspn - gets the length of a prefix substring.
- * @s: initial segment.
- * @accept: accepted bytes.
- * Return: the number of accepted bytes.
+ *_strspn -> gets the length of a prefix substring
+ *@s: string to be scanned
+ *@accept: string containing character to match
+ *Return: number of matching characters in the initial segment
  */
+
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j, bool;
+	int i, j, flag;
 
-	for (i = 0; *(s + i) != '\0'; i++)
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		bool = 1;
-		for (j = 0; *(accept + j) != '\0'; j++)
+		flag = 0;
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (*(s + i) == *(accept + j))
+			if (s[i] == accept[j])
 			{
-				bool = 0;
-				break;
+				count++;
+				flag = 1;
 			}
 		}
-		if (bool == 1)
-			break;
+		if (flag == 0)
+			return (count);
 	}
-	return (i);
+
+	return (0);
 }
